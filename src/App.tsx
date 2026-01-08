ad6b2d3ace098582116845c0d9059674f5c0df52
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2026-01-08 22:26:35 +0530

    [dasun] fix build issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import Home from "@/pages/home.tsx"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 export function App() {
+  const basename = import.meta.env.MODE === 'production' ? '/icrowd-fe' : '/'
+  
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
