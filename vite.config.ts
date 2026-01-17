152d88a4abfa318a1e7011efc83268d44867fc3a
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2026-01-17 18:20:31 +0530

    [dasun] fix firebase deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: process.env.NODE_ENV === "production" ? "/icrowd-fe/" : "/",
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
